3080e75a7a9640ba20e0d735a27bdd4943a6d632
kangpahul9 <pahulpreet2959@gmail.com>
2026-03-01 15:58:16 +1030

    Open day, withdrawal history,  upi autofill, close day denoms validation

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -10,6 +10,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false },
 });
 console.log("Connecting to DB:", process.env.DB_NAME);
 

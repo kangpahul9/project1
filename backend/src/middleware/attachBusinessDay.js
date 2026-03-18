a3d2cb97ba9e825b408c56c5b8bf6207bb887abd
kangpahul9 <pahulpreet2959@gmail.com>
2026-03-18 13:51:20 +1030

    Fixed

--- backend/src/middleware/attachBusinessDay.js
+++ backend/src/middleware/attachBusinessDay.js
@@ -1,17 +1,26 @@
-export async function attachBusinessDay(req,res,next){
-  const client = await pool.connect()
+import pool from "../config/db.js";
+import { getBusinessDay } from "../utils/getBusinessDay.js";
 
-  try{
+export async function attachBusinessDay(req, res, next) {
+
+  if (!req.restaurantId || !req.settings) {
+    return next();
+  }
+
+  const client = await pool.connect();
+
+  try {
     req.businessDayId = await getBusinessDay(
       client,
       req.restaurantId,
       req.settings,
-      req.user?.id   // ✅ ADD THIS
-    )
-    next()
-  } catch(err){
-    next(err)
-  } finally{
-    client.release()
+      req.user?.id
+    );
+
+    next();
+  } catch (err) {
+    return next(err);
+  } finally {
+    client.release();
   }
 }
